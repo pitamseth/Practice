f26e8292555750b779317e674f365efaf981764d
Pitam <pitamseth@gmail.com>
2022-07-02 10:53:33 +0530

    first commit

---
+++ pitam.java
@@ -0,0 +1,15 @@
+public class pitam {
+
+	public static void main(String[] args) {
+		int len;
+		len = args.length;
+		if (len == 2) {
+			System.out.println(args[0] + " technologies " + args[1]);
+		}
+		else {
+			System.out.println("Please give two argument");
+		}
+
+	}
+
+}
---
+++ pitam1.java
@@ -0,0 +1,10 @@
+public class pitam1 {
+
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			System.out.println("Welcome " + args[0]);
+		}
+
+	}
+
+}
